cb284cc7d2a8134bccf36d0f0534ba5ce3f67fec
walterlong <327519632@qq.com>
2019-12-09 09:44:43 +0800

    :recycle: 优化代码

--- vipmis-web/src/App.vue
+++ vipmis-web/src/App.vue
@@ -12,7 +12,7 @@
 
 <style>
 #app {
-	height: 100%;
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
