5a247ae2544fbc71df9f961419ee4e90eaa2639a
walterlong <327519632@qq.com>
2019-12-15 02:27:08 +0800

    :recycle: 不允许富文本编辑框传文件

--- vipmis-web/src/views/carddescription/CardDescription.vue
+++ vipmis-web/src/views/carddescription/CardDescription.vue
@@ -21,7 +21,29 @@
       return {
         loading: false,
         content: '',
-        editorOption: {}
+        editorOption: {
+          placeholder: 'Please enter it here...',
+          modules:{
+            toolbar:{
+              container: [
+                ['bold', 'italic', 'underline', 'strike'],// 加粗，斜体，下划线，删除线
+                ['blockquote'],// 引用
+                [{ 'header': 1 }, { 'header': 2 }],// 标题，键值对的形式；1、2表示字体大小
+                // eslint-disable-next-line standard/object-curly-even-spacing
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }],//列表
+                // eslint-disable-next-line standard/object-curly-even-spacing
+                [{ 'indent': '-1'}, { 'indent': '+1' }],// 缩进
+                [{ 'direction': 'rtl' }],// 文本方向
+                [{ 'size': ['small', false, 'large', 'huge'] }],// 字体大小
+                [{ 'header': [1, 2, 3, 4, 5, 6, false] }],//几级标题
+                [{ 'color': [] }, { 'background': [] }],// 字体颜色，字体背景颜色
+                [{ 'font': [] }],//字体
+                [{ 'align': [] }],//对齐方式
+                ['clean'],//清除字体样式
+              ]
+            }
+          }
+        }
       }
     },
     methods: {
